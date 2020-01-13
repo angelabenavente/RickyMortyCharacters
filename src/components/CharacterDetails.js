42ae6bac73282847be23416c329a66b3c9dd8e1e
angelabenavente <angela.b.almazan@gmail.com>
2020-01-13 19:11:31 +0100

    Add species icons

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import { Link, Route, Switch } from 'react-router-dom';
+import humanIcon from '../images/human.svg'
+import humanoidIcon from '../images/humanoide.svg'
+import alienIcon from '../images/alien.svg'
 
 const CharacterDetails = props => {
   const { image, name, species, origin, episode, status } = props.character;
@@ -9,13 +12,13 @@ const CharacterDetails = props => {
   return <div className="character-details-wrapper">
      <ul>
           <li>
-            <Link to="/">Volver</Link>
+            <Link to="/"><i class="fas fa-angle-left"></i> Volver</Link>
           </li>
         </ul>
     <img src={image} alt={name}></img>
     <h1>{name}</h1>
     <p>Status: {(status !== 'Dead') ? '' : <i className="fas fa-skull-crossbones"></i>} {status} </p>
-    <p>Species: {(species !== 'Human') ? <i className="fab fa-reddit-alien"></i> : <i className="fas fa-male"></i>} {species} </p>
+    <p>Species: {(species === 'Humanoid') ? <img className="specieIcon" src={humanoidIcon}/> : (species === 'Alien') ? <img className="specieIcon" src={alienIcon}/> : <img className="specieIcon" src={humanIcon}/>} {species} </p>
     <p>{episode}</p>
     {/* <p>{{origin}.name}</p> */}
     </div>
