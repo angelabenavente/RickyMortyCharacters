489b6905150ac5682c7252014f288e50f10acabf
angelabenavente <angela.b.almazan@gmail.com>
2020-01-13 20:57:59 +0100

    Add normalize

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Character = props => {
   return <div id={props.id} >
     <img className="characterImage" src={props.characterImg} alt={props.characterName}/>
+    <div className="characterDescription">
     <h3 className="characterName">{props.characterName}</h3>
     <p className="characterSpecie">{props.characterSpecies}</p>
+    </div>
   </div>
 }
 
