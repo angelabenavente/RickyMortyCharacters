f182a8fc7019fd7f51d9bc73e12d3126c372a259
angelabenavente <angela.b.almazan@gmail.com>
2020-01-14 10:14:48 +0100

    Change placeholder with onBlur event

--- src/components/CharacterFilter.js
+++ src/components/CharacterFilter.js
@@ -2,21 +2,44 @@ import React from 'react';
 import Header from './Header';
 
 
-const CharacterFilter = props => {
+class CharacterFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: "Search a character...",
+    }
 
-  const onChangeHandler = event => {
-    //event.preventDefault();
-    props.onChangeHandler(event.target.value);
+ 
+
   }
-  
+ 
+ 
+
+  render() {
+    const onFocusHandler = () => {
+      this.setState({
+        placeholder: ''
+      });
+    }
+
+    const onBlurHandler = () => {
+      this.setState({
+        placeholder: 'Search a character...'
+      });
+    }
+
+    const onChangeHandler = event => {
+      //event.preventDefault();
+      this.props.onChangeHandler(event.target.value);
+    }
   return (
     <React.Fragment>
       <Header/>
-      <form value={props.inputValue} className="filter-wrapper">
-      <input className="search-characters"  placeholder="&#xF002; Search a character..." style={{fontFamily: "Arial, FontAwesome"}} onChange={onChangeHandler} type='text' value={props.inputValue}></input>
+      <form value={this.props.inputValue} className="filter-wrapper">
+      <input onFocus={onFocusHandler} onBlur={onBlurHandler} className="search-characters"  placeholder={this.state.placeholder} onChange={onChangeHandler} type='text' value={this.props.inputValue}></input>
       </form>
     </React.Fragment>
   )
 }
-
+}
 export default CharacterFilter;
