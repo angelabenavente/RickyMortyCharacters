b4ee5257476ac908ff73d0df8cd64a4c5be54404
angelabenavente <angela.b.almazan@gmail.com>
2020-01-14 12:59:33 +0100

    Build docs

---
+++ docs/precache-manifest.b1da102a23c7b5fae2e470a49c51d9e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c3027914d1dbe0bd3518b5e2e4f9ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de3ea4cff2c487d872c4",
+    "url": "./static/css/main.63a493b9.chunk.css"
+  },
+  {
+    "revision": "d6eab07e3150be0935e5",
+    "url": "./static/js/2.a952a64e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.a952a64e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de3ea4cff2c487d872c4",
+    "url": "./static/js/main.9876d5d8.chunk.js"
+  },
+  {
+    "revision": "47c8a89bf13049df7ba7",
+    "url": "./static/js/runtime-main.b8ce57fa.js"
+  },
+  {
+    "revision": "c8624379f0f69d267437203f87472332",
+    "url": "./static/media/alien.c8624379.svg"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/headerImage.75509f2e.png"
+  },
+  {
+    "revision": "585b8a4c219c0110e6ff017634f8215f",
+    "url": "./static/media/humanoide.585b8a4c.svg"
+  }
+]);
