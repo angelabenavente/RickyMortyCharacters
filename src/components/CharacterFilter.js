f7d76955f8224636c8e6a91b8cd77d36b931368c
angelabenavente <angela.b.almazan@gmail.com>
2020-01-14 09:41:18 +0100

    Add icon in search input placeholder

--- src/components/CharacterFilter.js
+++ src/components/CharacterFilter.js
@@ -13,7 +13,7 @@ const CharacterFilter = props => {
     <React.Fragment>
       <Header/>
       <form value={props.inputValue} className="filter-wrapper">
-        <input className="search-characters" onChange={onChangeHandler} type='text' value={props.inputValue}></input>
+      <input className="search-characters"  placeholder="&#xF002; Search a character..." style={{fontFamily: "Arial, FontAwesome"}} onChange={onChangeHandler} type='text' value={props.inputValue}></input>
       </form>
     </React.Fragment>
   )
